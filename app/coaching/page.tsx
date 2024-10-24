ade7dfdc34d6d5a78bb87cb17b49624e4817f366
Shrey Kumar <shreykumar961@gmail.com>
2024-10-24 14:42:21 -0400

    Added pother pages

---
+++ app/coaching/page.tsx
@@ -0,0 +1,9 @@
+import Layout from "../components/Layout";
+
+export default function Coaching() {
+    return (
+        <Layout>
+            Coaching page
+        </Layout>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Layout from "./components/Layout";
 import SiteLink from "./components/SiteLink";
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <Layout>
       <h1 className="text-3xl mb-5">Hi I'm Shrey! 👋</h1>
-      <p>Welcome to my site! Feel free to get in touch with me <SiteLink href='/contact'>here</SiteLink> if you have any feedback on anything😃</p>
+      <p className="mb-12">Welcome to my site! Feel free to get in touch with me <SiteLink href='/contact'>here</SiteLink> if you have any feedback on anything😃</p>
+      <Image src="/images/dp.JPG" alt="Shrey" width={400} height={400} className="rounded" />
     </Layout>
   );
 }
