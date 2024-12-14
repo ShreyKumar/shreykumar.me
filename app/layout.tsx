0caebe17de0fe91598c22f1a09799bbe624533fe
ShreyKumar <shreykumar961@gmail.com>
2024-12-13 20:23:21 -0500

    Added Vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
 
@@ -26,6 +27,7 @@ export default function RootLayout({
             >
                 {children}
                 <SpeedInsights />
+                <Analytics />
             </body>
         </html>
     );
