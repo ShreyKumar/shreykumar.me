781d4a10ad725529c7b58cdae7ff957d83eb23ac
ShreyKumar <shreykumar961@gmail.com>
2024-12-17 14:19:25 -0500

    Made border a bit lighter

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className="border-t mt-10 py-6 flex flex-col md:flex-row justify-center w-3/4 mx-auto text-center text-xs text-gray-400 border-gray-400">
+        <footer className="border-t mt-10 py-6 flex flex-col md:flex-row justify-center w-3/4 mx-auto text-center text-xs text-gray-400 border-gray-200">
             <p className="mr-1">Built with the Next.js App Router.</p>
             <p>No rights reserved.</p>
         </footer>
